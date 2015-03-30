6296b1626da0c547b6668a6d9c18d180a37c325c
dan <dan@contatta.com>
2015-03-29 19:31:30 -0600

    have bot try action and query events

--- eddy/bin/bot.py
+++ eddy/bin/bot.py
@@ -3,17 +3,23 @@ from eddy.module.query import QueryEvent
 from eddy.parser import Parser
 
 parser = Parser.Parser()
-#action = ActionEvent.ActionEvent(parser)
-action =QueryEvent.QueryEvent(parser)
+events = [
+    ActionEvent.ActionEvent(parser),
+    QueryEvent.QueryEvent(parser)
+]
 print 'Eddy>> What would you like?'
 print 'Eddy>> You can say \'invite @dan to room\'...'
 while True:
     try:
         var = raw_input("user>> ")
-        response = action.find(var)
-        if response.result:
-            print 'Eddy>> ' + str(response.data)
-        else:
+        responded = False
+        for e in events:
+            response = e.find(var)
+            if response.result:
+                print 'Eddy>> I found event:' + str(response.event) + ' with ' + str(response.data)
+                responded = True
+                break
+        if not responded:
             print "Huh?"
     except Exception, ex:
         print ex
